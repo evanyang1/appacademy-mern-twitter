dc91323ca669c98b0f06864c7c62c63beaae783f
Evan Yang <evanyang@pop-os.localdomain>
2019-11-25 23:47:43 -0800

    added some routes and user model

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const db = require('./config/keys').mongoURI
+const users = require('./routes/api/users')
+const tweets = require('./routes/api/tweets')
 
 mongoose.connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB"))
@@ -10,6 +12,9 @@ mongoose.connect(db, { useNewUrlParser: true })
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
+
+app.use("/api/users", users)
+app.use("/api/tweets", tweets)
 
 const port = process.env.PORT || 5000
 
